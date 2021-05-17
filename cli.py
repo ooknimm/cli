b33f07ca734b216f09f2d22f7d834b99ae4c26f4
ooknimm <ooknimm@gmail.com>
2021-05-18 01:28:53 +0900

    ff

--- cli.py
+++ cli.py
@@ -1,25 +1,19 @@
 import click
 import os
+from collections import defaultdict, Counter
 
-@click.command()
-@click.argument('files', type=click.Path(exists=True))
-def main(files):
-    """Print all FILES file names."""
-    path_list = files.split('/')
-    if not '.rule' in path_list[-2]:
-        return '룰셋이여야합니다'
 
-    sort_dir = sorted(os.listdir(files))   
-    print(sort_dir) 
-    value, value2 = map(int, click.prompt('변경 범위를 입력하세요.', type=str).split(','))
+def dir_make(path):
+    sort_dir = sorted(os.listdir(path))   
+    print(', '.join(sort_dir)) 
+    start, end = map(int, click.prompt('변경 범위를 입력하세요', type=str).split(','))
 
-    from collections import defaultdict, Counter
     table = defaultdict(list)
-    for i in range(value, value2+1):
-        a = '{:0>3}'.format(i)
-        for s in sort_dir:
-            if a in s:
-                table[a].append(s)
+    for i in range(start, end+1):
+        number = '{:0>3}'.format(i)
+        for dir in sort_dir:
+            if number in dir:
+                table[number].append(dir)
     
     for key in table.keys():
         temp = []
@@ -30,28 +24,53 @@ def main(files):
                     key,
                     '\n'.join(f'{idx: >4}: {c}' for idx, c in enumerate(table[key], start=1))
                 )
-                op = click.prompt(prompt_text, type=prompt_type, show_choices=False)
-                temp.append(table[key][op-1])
-                print(temp)
-                del table[key][op-1]
+                select = click.prompt(prompt_text, type=prompt_type, show_choices=False)
+                temp.append(table[key][select-1])
+                del table[key][select-1]
                 if len(table[key]) == 1:
                     temp.append(table[key][0])
                     print(temp)
             table[key] = temp
 
     op = click.prompt('증가량을 입력해주세요', type=int)
+
     count = 0
-    for values in table.values():
-        i = count
-        for value in values:
-            y = value.split('-')
-            b = int(y[0])
-            x = '{:0>3}'.format(b+op+i)
-            os.rename(files + '/' + value, files + '/' + x + '-' + y[1])
-            i += 1
-        count += i - count - 1
+    try:
+        for values in table.values():
+            i = count
+            for value in values:
+                names = value.split('-')
+                old_n = int(names[0])
+                new_n = '{:0>3}'.format(old_n+op+i)
+                os.rename('{}/{}'.format(path, value), '{}/{}-{}'.format(path, new_n, names[1]))
+                i += 1
+            count += i - count - 1
+    except OSError as e:
+        filename_1 = e.filename.split('/')[-1]
+        filename_2 = e.filename2.split('/')[-1]
+        print('{}, {}'.format(filename_1, filename_2))
+        print('중복되는 요소가 있습니다.')
+        return
     
-    print(sorted(os.listdir(files)))
+    print(sorted(os.listdir(path)))
+
+def csv_make(path):
+    pass
+
+@click.command()
+@click.argument('path', type=click.Path(exists=True))
+def main(path):
+    """디렉토리 넘버링을 변경합니다."""
+    path_list = path.split('/')
+
+    if '.csv' in path_list[-1]:
+        return csv_make(path)
+
+    if not '.rule' in path_list[-2]:
+        print('룰셋이여야합니다')
+        return
+    
+    return dir_make(path)
     
 
 if __name__ == '__main__':
